c4101abb2ae841adb260dd19793067f9b40cca91
Tejas <tejas75o25@gmail.com>
2022-10-06 21:38:25 +0530

    Added Packages & Dependencies & made a simple program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    // cursor::{Hide, MoveTo, MoveToColumn, MoveUp, RestorePosition, SavePosition},
+    // event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
+    // style::{Print, Stylize},
+    execute,
+    queue,
+    terminal::{self, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{
+    io::{self, stdout, Write},
+    time::{Duration, Instant},
+};
+
+fn main() -> io::Result<()> {
+    execute!(stdout(), EnterAlternateScreen)?;
+    let now = Instant::now();
+    
+    for i in 0..5_000 {
+        println!("{}", i);
+        // execute!(stdout(), Clear(ClearType::FromCursorUp))?;
+    }
+    
+    execute!(stdout(), Clear(ClearType::FromCursorUp))?;
+    let elapsed_time = now.elapsed();
+
+    println!("Hello World!\nfunction took {} seconds.", elapsed_time.as_secs());
+
+
+    // execute!(stdout(), LeaveAlternateScreen)?;
+
+    Ok(())
 }
