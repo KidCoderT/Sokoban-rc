8365e9656b73a05dea32b55058f3594a013203bb
Tejas <tejas75o25@gmail.com>
2022-10-07 19:56:23 +0530

    Figured Out coloring text and found needed colors

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use ansi_term;
+use ansi_term::*;
 use crossterm::{
     cursor,
     event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
     execute, queue,
-    style::{Print, Stylize},
+    style::Print,
     terminal,
 };
 use std::{
     io::{self, stdout, Write},
-    time::{Duration, Instant},
+    time::{Duration, Instant}, str::FromStr,
 };
 
 struct Point {
@@ -40,22 +40,42 @@ fn padding(window_size: u16, text_size: u16) -> u16 {
     (window_size - text_size) / 2
 }
 
+fn get_style(string: &str) -> Style {
+    // Gets the Style for the Characters
+    match string {
+        "@" => Color::White.on(Color::RGB(232,207,0)), // Player
+        "*" => Color::White.on(Color::RGB(22,69,142)), // Wall
+        "#" => Color::White.on(Color::RGB(243,47,55)), // Crate
+        "?" => Color::Green.on(Color::Black), // Storage Loc
+        "." => Color::Black.on(Color::Black), // Blank
+        _ => Color::Green.on(Color::RGB(33,135,33)) // correct pos
+    }
+}
+ 
 fn main() -> io::Result<()> {
     execute!(
         stdout(),
         terminal::EnterAlternateScreen,
-        cursor::Hide,
-        cursor::MoveTo(0, 0)
+        cursor::Hide
     )?;
+
     terminal::enable_raw_mode()?;
-    let _ = ansi_term::enable_ansi_support();
+    let _ = enable_ansi_support();
 
     // execute!(stdout(), terminal::SetSize(200, 200))?;
     execute!(stdout(), terminal::SetTitle("Sokoban - RC"))?;
     let (columns, rows) = terminal::size().unwrap();
 
-    let text = format!("hello {columns} {rows}");
-    execute!(stdout(), Print(text)).expect("Problem");
+    let text = format!("columns: {columns}, rows: {rows}");
+    execute!(stdout(), Print(text))?;
+
+    // Printing all the colors
+    execute!(stdout(), Print(get_style("@").paint("\n    Player    ")))?;
+    execute!(stdout(), Print(get_style("*").paint("\n    Wall    ")))?;
+    execute!(stdout(), Print(get_style("#").paint("\n    Crate    ")))?;
+    execute!(stdout(), Print(get_style(".").paint("\n    Blank    ")))?;
+    execute!(stdout(), Print(get_style("?").paint("\n    Storage Loc ???")))?;
+    execute!(stdout(), Print(get_style("-").paint("\n    Correct Loc")))?;
 
     // execute!(stdout(), terminal::LeaveAlternateScreen)?;
     execute!(stdout(), cursor::Show)?;
